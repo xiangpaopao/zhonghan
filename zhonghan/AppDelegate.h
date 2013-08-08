6d355112cf8a5b6f7a49c8c85ce4292e3d524e9d
xiangpaopao <xiangpaopao@gmail.com>
2013-08-08 09:56:45 +0800

    zhonghan
    zhonghan

---
+++ zhonghan/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  zhonghan
+//
+//  Created by xiangpaopao on 13-8-6.
+//  Copyright (c) 2013年 xiangpaopao. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ zhonghan/FirstViewController.h
@@ -0,0 +1,13 @@
+//
+//  FirstViewController.h
+//  zhonghan
+//
+//  Created by xiangpaopao on 13-8-6.
+//  Copyright (c) 2013年 xiangpaopao. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface FirstViewController : UIViewController
+
+@end
